66e5940f427782da3951de1d682dd67c38e30680
Karl Jerico <152370707+karl-jerico@users.noreply.github.com>
2023-11-30 13:55:15 +0800

    Revert "Revert "Chore/initial routes""

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import * as path from 'path'
 
 dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   server: {
     port: Number(process.env.PORT)
   },
