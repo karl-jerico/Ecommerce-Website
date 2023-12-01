b0a97d7946766c038f7703215c562572103cc7da
karl-jerico <jerico.jeks@gmail.com>
2023-12-01 11:43:07 +0800

    Add subscribe feature

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -3,6 +3,7 @@ import Hero from '@/components/shop/hero'
 import Popular from '@/components/shop/popular'
 import Offers from '@/components/shop/offers'
 import NewCollections from '@/components/shop/newCollections'
+import NewsLetter from '../components/shop/newsLetter'
 
 const Shop = () => {
   return (
@@ -11,6 +12,7 @@ const Shop = () => {
       <Popular />
       <Offers />
       <NewCollections />
+      <NewsLetter />
     </div>
   )
 }
