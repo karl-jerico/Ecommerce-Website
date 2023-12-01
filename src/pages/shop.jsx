f9ba0aea85477c94781058597a4f1b2c0a10eeb5
karl-jerico <jerico.jeks@gmail.com>
2023-12-01 11:53:47 +0800

    Add new line

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -3,7 +3,7 @@ import Hero from '@/components/shop/hero'
 import Popular from '@/components/shop/popular'
 import Offers from '@/components/shop/offers'
 import NewCollections from '@/components/shop/newCollections'
-import NewsLetter from '../components/shop/newsLetter'
+import NewsLetter from '@/components/shop/newsLetter'
 
 const Shop = () => {
   return (
