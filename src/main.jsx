a5e012eaa003038100e1dab901c17d78b7610bba
karl-jerico <jerico.jeks@gmail.com>
2023-12-02 21:47:27 +0800

    Add shop category

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
+import ShopContextProvider from './context/shopContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </React.StrictMode>,
 )
--- src/pages/shopCategory.jsx
+++ src/pages/shopCategory.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { shopContext } from '@/context/shopContext';
+import dropdown_icon from '@/assets/dropdown_icon.png'
+import Item from '@/components/shop/item'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const {all_products} = useContext(shopContext);
   return (
-    <div>ShopCategory</div>
+    <section className="shop-category">
+      <img className='shopcategory-banner' src={props.banner} alt="banner" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="dropdown_icon" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_products.map((item, i)=>{
+          if (props.category===item.category) {
+            return <Item key={i} id={item.id} name={item.name} 
+            image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+          }
+          else{
+            return null;
+          }
+        })}
+      </div>
+    </section>
   )
 }
 
