1b96a7cd6feacf63065a333eddd0bd537e32a24e
karl-jerico <jerico.jeks@gmail.com>
2023-12-04 11:12:11 +0800

    Add description box

---
+++ src/components/products/descriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const DescriptionBox = () => {
+  return (
+    <section className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-desc">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi, corrupti quod quaerat architecto ipsa soluta, 
+                quibusdam tenetur blanditiis est fuga quas tempore possimus 
+                porro, eum temporibus ipsam numquam assumenda quam. Lorem ipsum
+                dolor sit amet consectetur, adipisicing elit. Alias, nam eum!
+                Ut unde perspiciatis eaque ducimus sit! Explicabo, error repudiandae!</p>
+        </div>
+    </section>
+  )
+}
+
+export default DescriptionBox
