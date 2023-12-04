c9fe9216f2c3cc1ef0464ae4c08be3b3b254a743
karl-jerico <jerico.jeks@gmail.com>
2023-12-04 11:13:00 +0800

    Add product feature

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { shopContext } from '@/context/shopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '@/components/products/breadcrum';
+import ProductDisplay from '@/components/products/productDisplay';
+import DescriptionBox from '@/components/products/descriptionBox';
+import RelatedProducts from '../components/products/relatedProducts';
 
 const Product = () => {
+  const {all_products} = useContext(shopContext);
+  const {productId} = useParams();
+  const product = all_products.find((e) => e.id === Number(productId));
   return (
-    <div>Product</div>
+    <div>
+    <Breadcrum product={product} />
+    <ProductDisplay product={product} />
+    <DescriptionBox />
+    <RelatedProducts />
+    </div>
   )
 }
 
